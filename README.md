e3e3c09b09167b096ed4f7bb9c8d6be5554c2363
chefhoobajoob <chefhoobajoob@gmail.com>
2021-09-15 08:31:46 -0700

    Added support for customizing jvm test args in gradle tasks (was previously doing this in the IDE). With these changes, the esx VerticleFactory is created successfully

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # es4x-graal-js-error-reproducer
 simple reproducer to generate graalvm error running on jdk 11: java.lang.NoClassDefFoundError: Could not initialize class org.graalvm.polyglot.Engine$ImplHolder
+
+**EDIT**: after revising the build script to automate JVM args to include the graal compiler modules for running test cases, this revision eliminates the error. Earlier tests run via the IDE must have been setting module path properties incorrectly.
--- src/test/java/org/acme/Tests.java
+++ src/test/java/org/acme/Tests.java
@@ -5,14 +5,19 @@ import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(VertxExtension.class)
 public class Tests {
+    Logger _logger = LoggerFactory.getLogger( Tests.class );
+
     @Test
     public void test( Vertx theVertx, VertxTestContext theContext )
     {
+        _logger.info("Test is running!");
         assertThat( true ).isTrue();
         theContext.completeNow();
     }
